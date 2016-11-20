d3fc56991c5770ae6b48d3435c7174b9a151f197
Chris Hayes <chris@moderncognition.com>
2016-11-20 16:52:54 -0500

    Import tribute bower dependencies

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-tribute-shim'
+  name: 'ember-cli-tribute-shim',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import('bower_components/tribute/dist/tribute.js');
+    app.import('bower_components/tribute/dist/tribute.css');
+  }
 };
