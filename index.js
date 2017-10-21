81bdf1ec635e3a22e902317ee9e44d4f8787fc17
nikz <me@nikwakelin.com>
2017-10-21 13:36:09 +0100

    Fixes deprecation warnings

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'ember-cli-tribute-shim',
 
   included: function(app) {
-    this._super.included(app);
+    this._super(app);
 
     app.import('bower_components/tribute/dist/tribute.js');
     app.import('bower_components/tribute/dist/tribute.css');
