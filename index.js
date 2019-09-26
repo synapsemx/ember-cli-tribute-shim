b8a3296e3d3b3326d81b6c24816bc885675077fa
Shane Ballman <shane.ballman@synapsemx.com>
2019-09-25 23:47:57 -0400

    Successfully builds using npm now

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ var MergeTrees = require('broccoli-merge-trees');
 module.exports = {
   name: 'ember-cli-tribute-shim',
 
-  included: function(app) {
+  included() {
     this._super.included.apply(this, arguments);
 
     this.import('vendor/tribute.js');
     this.import('vendor/tribute.css');
     this.import('vendor/shims/tribute.js');
-  }
+  },
 
   treeForVendor(vendorTree) {
-    var tributeTree = new Funnel(path.dirname(require.resolve('tributejs/tribute.js')), {
+    var tributeTree = new Funnel(path.dirname(require.resolve('tributejs')), {
       files: ['tribute.js', 'tribute.css'],
     });
 
